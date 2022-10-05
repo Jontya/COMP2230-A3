88af0fd9f5caed81c15aafc043cb70e32ee72478
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-06 01:54:06 +1100

    Added extra param

--- src/Fixed.java
+++ src/Fixed.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
 
 public class Fixed extends CPU{
+    private int numFramesPerProcess;
+
     public Fixed(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
+
+        numFramesPerProcess = _frames / processList.size();
     }
 
     @Override
