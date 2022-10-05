56e01d24064c198de813ced42235e06a2fb5e540
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-06 00:42:13 +1100

    Child class structure for each algo

---
+++ src/Fixed.java
@@ -0,0 +1,17 @@
+public class Fixed extends CPU{
+    public Fixed(int _frames, int _timeQuantum, String _algoName){
+        super(_timeQuantum, _algoName);
+    }
+
+    @Override
+    protected void addToMainMem(Page page) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void checkInMainMem(Process process) {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,18 @@
+public class Variable extends CPU{
+    
+    public Variable(int _timeQuantum, String _algoName){
+        super(_timeQuantum, _algoName);
+    }
+
+    @Override
+    protected void addToMainMem(Page page) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void checkInMainMem(Process process) {
+        // TODO Auto-generated method stub
+        
+    }
+}
