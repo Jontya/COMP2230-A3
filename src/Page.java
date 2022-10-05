81c35aac688b7d82c822e3904413da89abea5fdc
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-06 01:53:42 +1100

    Added basic Page class layout

--- src/Page.java
+++ src/Page.java
@@ -1,3 +1,17 @@
 public class Page {
-    
+    private int pageID;
+    private String processName;
+
+    public Page(int _pageID, String _processName){
+        pageID = _pageID;
+        processName  = _processName;
+    }
+
+    public int getPageID(){
+        return pageID;
+    }
+
+    public String getProcessName(){
+        return processName;
+    }
 }
