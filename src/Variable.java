22f58a14e75e1a7122bc59ced2f589e2290e9565
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-06 01:53:01 +1100

    Update Variable.java

--- src/Variable.java
+++ src/Variable.java
@@ -1,20 +1,17 @@
 import java.util.ArrayList;
 
 public class Variable extends CPU{
-    
     public Variable(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
     }
 
     @Override
     protected void addToMainMem(Page page) {
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     protected void checkInMainMem(Process process) {
-        // TODO Auto-generated method stub
         
     }
 }
