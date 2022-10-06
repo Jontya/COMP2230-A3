69c9465add28385a17d8b8000f6f09cb24cc1593
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-06 21:33:22 +1100

    Added method to check main memory

--- src/Fixed.java
+++ src/Fixed.java
@@ -1,23 +1,39 @@
 import java.util.ArrayList;
-
+import java.util.HashMap;
 public class Fixed extends CPU{
-    private int numFramesPerProcess;
+    private int numAllocatedFrames;
+    private HashMap<String, ArrayList<Page>> mainMemory;
 
     public Fixed(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
 
-        numFramesPerProcess = _frames / processList.size();
+        mainMemory = new HashMap<>();
+        numAllocatedFrames = _frames / processList.size();
     }
 
     @Override
-    protected void addToMainMem(Page page) {
+    protected void addToMainMem(Page process) {
         // TODO Auto-generated method stub
         
     }
 
     @Override
-    protected void checkInMainMem(Process process) {
-        // TODO Auto-generated method stub
-        
+    protected boolean checkInMainMem(Process process) {
+        if(mainMemory.containsKey(process.getProcessName())){
+            ArrayList<Page> temp = mainMemory.get(process.getProcessName());
+            for(int i = 0; i < temp.size(); i++){
+                if(temp.get(i).getpageInstruction() == process.getCurrentInstruction()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void removeProcessFromMemory(Process process) {
+        if(mainMemory.containsKey(process.getProcessName())){
+            mainMemory.remove(process.getProcessName());
+        }
     }
 }
