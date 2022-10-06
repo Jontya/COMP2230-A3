8fb338a9a4f3e1eaf6f1e320f3d46f0755089c63
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-07 03:02:41 +1100

    Added prime ready queue method

--- src/Fixed.java
+++ src/Fixed.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+
 public class Fixed extends CPU{
     private int numAllocatedFrames;
-    private HashMap<String, ArrayList<Page>> mainMemory;
+    protected HashMap<String, ArrayList<Page>> mainMemory;
 
     public Fixed(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
@@ -11,18 +12,45 @@ public class Fixed extends CPU{
         numAllocatedFrames = _frames / processList.size();
     }
 
-    @Override
-    protected void addToMainMem(Page process) {
-        // TODO Auto-generated method stub
-        
+    public void primeReadyQueue(){
+        for(int i = 0; i < processList.size(); i++){
+            readyQueue.add(processList.get(i));
+            mainMemory.put(processList.get(i).getProcessName(), new ArrayList<Page>());
+        }
     }
 
     @Override
-    protected boolean checkInMainMem(Process process) {
-        if(mainMemory.containsKey(process.getProcessName())){
-            ArrayList<Page> temp = mainMemory.get(process.getProcessName());
+    protected void loadIntoMemory() {
+        if(mainMemory.get(currProcess.getProcessName()).size() < numAllocatedFrames){
+            mainMemory.get(currProcess.getProcessName()).add(new Page(currProcess.getCurrentInstruction()));
+        }
+        else{
+            ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
+            boolean replaced = false;
+            while(!replaced){
+                for(int i = 0; i < temp.size(); i++){
+                    if(temp.get(i).getReferenceCounter() == 0){
+                        temp.remove(i);
+                        temp.add(new Page(currProcess.getCurrentInstruction())); 
+                        replaced = true;
+                        break;
+                    }
+                    else{
+                        temp.get(i).decReferenceCounter();
+                    }
+                }
+            }
+            mainMemory.replace(currProcess.getProcessName(), temp);
+        }
+    }
+
+    @Override
+    protected boolean checkMemory() {
+        if(mainMemory.containsKey(currProcess.getProcessName())){
+            ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
             for(int i = 0; i < temp.size(); i++){
-                if(temp.get(i).getpageInstruction() == process.getCurrentInstruction()){
+                if(temp.get(i).getpageInstruction() == currProcess.getCurrentInstruction()){
+                    temp.get(i).incReferenceCounter();
                     return true;
                 }
             }
@@ -31,9 +59,7 @@ public class Fixed extends CPU{
     }
 
     @Override
-    protected void removeProcessFromMemory(Process process) {
-        if(mainMemory.containsKey(process.getProcessName())){
-            mainMemory.remove(process.getProcessName());
-        }
+    protected void removeFromMemory() {
+    
     }
 }
