90ee902e5751a2abb9b916c5c2eb11f9d384711d
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-07 03:02:29 +1100

    Updated method names

--- src/CPU.java
+++ src/CPU.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.HashMap;
 
 public abstract class CPU {
     protected int currTime;
     protected int timeQuantum;
     protected String algoName;
     protected Queue<Process> readyQueue;
-    protected Queue<Process> blockedQueue;
+    protected ArrayList<Process> blockedQueue;
     protected ArrayList<Process> processList;
     protected PriorityQueue<Process> finishedList;
-
-    private Process currProcess;
+    protected Process currProcess;
 
     public CPU (int _timeQuantum, String _algoName, ArrayList<Process> _processList){
         currTime = 0;
@@ -20,28 +20,30 @@ public abstract class CPU {
         timeQuantum = _timeQuantum;
         processList = _processList;
         readyQueue = new LinkedList<>();
-        blockedQueue = new LinkedList<>();
+        blockedQueue = new ArrayList<>();
         finishedList = new PriorityQueue<>();
     }
 
     public void run(){
-        for(int i = 0; i < processList.size(); i++){
-            readyQueue.add(processList.get(i));
-        }
-
-        // Round Robin Scheduling Algorithm
         while(finishedList.size() != processList.size()){
             
         }
     }
 
-    public void blockCurrProcess(){
-        currProcess.newFault(currTime);
-        if(!currProcess.getStatus().equals("blocked")){
-            currProcess.swapStatus();
+    public void checkBlockedQueue(){
+        if(!blockedQueue.isEmpty()){
+            ArrayList<Process> temp = new ArrayList<>();
+            int size = blockedQueue.size();
+            for(int i = 0; i < size; i++){
+                if(blockedQueue.get(i).getBlockedTime() == currTime){
+                    temp.add(blockedQueue.get(i));
+                }
+            }
+
+            for(int i = 0; i < temp.size(); i++){
+                readyQueue.add(temp.get(i));
+            }
         }
-        readyQueue.remove();
-        blockedQueue.add(currProcess);
     }
 
     public String getSimulationReport(){
@@ -54,10 +56,10 @@ public abstract class CPU {
         return out;
     }
 
-    protected abstract void addToMainMem(Page page);
+    protected abstract void loadIntoMemory();
 
-    protected abstract boolean checkInMainMem(Process process);
+    protected abstract boolean checkMemory();
 
-    protected abstract void removeProcessFromMemory(Process process);
+    protected abstract void removeFromMemory();
 
 }
