6d4f79cd45ab5151fc9dcabdbd2f660bfd6096d8
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-08 02:07:05 +1100

    Updated scheduling algo

--- src/CPU.java
+++ src/CPU.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.HashMap;
+
 
 public abstract class CPU {
     protected int currTime;
-    protected int timeQuantum;
     protected String algoName;
+    protected int timeQuantum;
+    protected Process currProcess;
     protected Queue<Process> readyQueue;
-    protected ArrayList<Process> blockedQueue;
+    protected Queue<Process> blockedQueue;
     protected ArrayList<Process> processList;
     protected PriorityQueue<Process> finishedList;
-    protected Process currProcess;
 
     public CPU (int _timeQuantum, String _algoName, ArrayList<Process> _processList){
         currTime = 0;
@@ -20,40 +20,75 @@ public abstract class CPU {
         timeQuantum = _timeQuantum;
         processList = _processList;
         readyQueue = new LinkedList<>();
-        blockedQueue = new ArrayList<>();
+        blockedQueue = new LinkedList<>();
         finishedList = new PriorityQueue<>();
     }
 
     public void run(){
         while(finishedList.size() != processList.size()){
-            
+            if(readyQueue.isEmpty()){
+                if(blockedQueue.isEmpty()){
+                    return;
+                }
+                currTime = blockedQueue.peek().getNextEvent();
+                checkBlockedQueue();
+            }
+
+            currProcess = readyQueue.remove();
+            if(currProcess.getProcessStatus().equals("ready")){
+                currProcess.nextInstruction();
+            }
+            else{
+                currProcess.swapStatus();
+            }
+
+            for(int i = 0; i < timeQuantum; i++){
+                if(!checkMemory()){
+                    loadIntoMemory();
+                    currProcess.swapStatus();
+                    currProcess.setNextEvent(currTime + 6);
+                    currProcess.addNewPageFault(currTime);
+                    blockedQueue.add(currProcess);
+                    break;
+                }
+
+                currTime++;
+                if(!blockedQueue.isEmpty()){
+                    checkBlockedQueue();
+                }
+
+                if(currProcess.isFinished()){
+                    currProcess.setFinishedTime(currTime);
+                    finishedList.add(currProcess);
+                    break;
+                }
+                else{
+                    if(i == timeQuantum - 1){
+                        readyQueue.add(currProcess);
+                    }
+                    else{
+                        currProcess.nextInstruction();
+                    }
+                }
+            }
         }
     }
 
     public void checkBlockedQueue(){
-        if(!blockedQueue.isEmpty()){
-            ArrayList<Process> temp = new ArrayList<>();
-            int size = blockedQueue.size();
-            for(int i = 0; i < size; i++){
-                if(blockedQueue.get(i).getBlockedTime() == currTime){
-                    temp.add(blockedQueue.get(i));
+        boolean checked = false;
+        while(!checked){
+            if(!blockedQueue.isEmpty()){
+                if(blockedQueue.peek().getNextEvent() == currTime){
+                    readyQueue.add(blockedQueue.remove());
+                }
+                else{
+                    checked = true;
                 }
             }
-
-            for(int i = 0; i < temp.size(); i++){
-                readyQueue.add(temp.get(i));
+            else{
+                checked = true;
             }
         }
-    }
-
-    public String getSimulationReport(){
-        String out =  "GCLOCK - " + algoName + " Replacement:\n";
-        out += "PID  Process Name      Turnaround Time  # Faults  Fault Times\n";
-        while(!finishedList.isEmpty()){
-            Process temp = finishedList.remove();
-            out += String.format("%-4d %-17s %-16d %-9d %-4s", temp.getProcessID(), temp.getProcessName(), temp.getTimeFinished(), temp.getNumFaults(), temp.getFaultTimes()) + "\n";
-        }
-        return out;
     }
 
     protected abstract void loadIntoMemory();
@@ -61,5 +96,17 @@ public abstract class CPU {
     protected abstract boolean checkMemory();
 
     protected abstract void removeFromMemory();
+
+
+    
+    public String getSimulationReport(){
+        String out =  "GCLOCK - " + algoName + " Replacement:\n";
+        out += "PID  Process Name      Turnaround Time  # Faults  Fault Times\n";
+        while(!finishedList.isEmpty()){
+            Process temp = finishedList.remove();
+            out += String.format("%-4d %-17s %-16d %-9d %-4s", temp.getProcessID(), temp.getProcessName(), temp.getTimeFinished(), temp.getPageFaultCount(), temp.getFaultTimes()) + "\n";
+        }
+        return out;
+    }
 
 }
