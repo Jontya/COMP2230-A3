e08bbe9df6fca4e002d46779399d2acb664ee522
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-09 23:42:47 +1100

    Final Fixed algorithm

--- src/Fixed.java
+++ src/Fixed.java
@@ -3,18 +3,16 @@ import java.util.HashMap;
 
 public class Fixed extends CPU{
     private int numAllocatedFrames;
-    private int currPointer;
     private HashMap<String, ArrayList<Page>> mainMemory;
 
     public Fixed(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
-
-        currPointer = 0;
         mainMemory = new HashMap<>();
         numAllocatedFrames = _frames / processList.size();
     }
 
-    public void primeReadyQueue(){
+    @Override
+    protected void primeReadyQueue() {
         for(int i = 0; i < processList.size(); i++){
             readyQueue.add(processList.get(i));
             mainMemory.put(processList.get(i).getProcessName(), new ArrayList<Page>());
@@ -30,20 +28,20 @@ public class Fixed extends CPU{
             ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
             boolean replaced = false;
             while(!replaced){
-                if(temp.get(currPointer).getReferenceCounter() == 0){
-                    temp.set(currPointer, new Page(currProcess.getCurrentInstruction()));
+                if(temp.get(currClockPointer).getReferenceCounter() == 0){
+                    temp.set(currClockPointer, new Page(currProcess.getCurrentInstruction()));
                     replaced = true;
                 }
                 else{
-                    temp.get(currPointer).decReferenceCounter();
+                    temp.get(currClockPointer).decReferenceCounter();
                 }
 
                 if(!replaced){
-                    if(currPointer == temp.size() - 1){
-                        currPointer = 0;
+                    if(currClockPointer == temp.size() - 1){
+                        currClockPointer = 0;
                     }
                     else{
-                        currPointer++;
+                        currClockPointer++;
                     }
                 }
             }
@@ -52,7 +50,7 @@ public class Fixed extends CPU{
     }
 
     @Override
-    protected boolean checkMemory() {
+    protected boolean checkMemory(){
         if(mainMemory.containsKey(currProcess.getProcessName())){
             ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
             for(int i = 0; i < temp.size(); i++){
@@ -67,6 +65,9 @@ public class Fixed extends CPU{
 
     @Override
     protected void removeFromMemory() {
-    
+        if(mainMemory.containsKey(currProcess.getProcessName())){
+            mainMemory.remove(currProcess.getProcessName());
+        }
+        
     }
 }
