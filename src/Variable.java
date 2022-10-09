a11d1d65c2d37bf6fe4fd3c08ed355d4e66f60e4
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-09 23:43:07 +1100

    Base class

--- src/Variable.java
+++ src/Variable.java
@@ -1,25 +1,70 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Variable extends CPU{
+    private ArrayList<Page> mainMemory;
+    private int frames;
+
     public Variable(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
+        mainMemory = new ArrayList<>();
+        frames = _frames;
+    }
+
+    @Override
+    protected void primeReadyQueue() {
+        for(int i = 0; i < processList.size(); i++){
+            readyQueue.add(processList.get(i));
+        }
     }
 
     @Override
     protected void loadIntoMemory() {
-        // TODO Auto-generated method stub
+        if(frames != 0){
+            mainMemory.add(new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName()));
+            frames--;
+        }
+        else{
+            boolean replaced = false;
+            while(!replaced){
+                for(int i = 0; i < mainMemory.size(); i++){
+                    if(mainMemory.get(currClockPointer).getReferenceCounter() == 0){
+                        mainMemory.set(currClockPointer, new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName()));
+                        frames--;
+                        replaced = true;
+                        break;
+                    }
+                    else{
+                        mainMemory.get(currClockPointer).decReferenceCounter();
+                    }
+
+                    if(currClockPointer == mainMemory.size() - 1){
+                        currClockPointer = 0;
+                    }
+                    else{
+                        currClockPointer++;
+                    }
+                }
+            }
+        }
         
     }
 
     @Override
     protected boolean checkMemory() {
-        // TODO Auto-generated method stub
+        for(int i = 0; i < mainMemory.size(); i++){
+            if(mainMemory.get(i).getProcessName().equals(currProcess.getProcessName())){
+                if(mainMemory.get(i).getpageInstruction() == currProcess.getCurrentInstruction()){
+                    mainMemory.get(i).incReferenceCounter();
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     protected void removeFromMemory() {
-        // TODO Auto-generated method stub
         
     }
 }
