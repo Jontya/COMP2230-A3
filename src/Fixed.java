9ab14a014678e40b458ca46181662fbbe4103fb0
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-10-18 16:47:48 +1100

    Refactor

--- src/Fixed.java
+++ src/Fixed.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 public class Fixed extends CPU{
     private int numAllocatedFrames;
-    private HashMap<String, ArrayList<Page>> mainMemory;
+    private HashMap<String, GCLOCK> mainMemory;
 
     public Fixed(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
@@ -15,59 +15,32 @@ public class Fixed extends CPU{
     protected void primeReadyQueue() {
         for(int i = 0; i < processList.size(); i++){
             readyQueue.add(processList.get(i));
-            mainMemory.put(processList.get(i).getProcessName(), new ArrayList<Page>());
+            mainMemory.put(processList.get(i).getProcessName(), new GCLOCK());
         }
     }
 
     @Override
     protected void loadIntoMemory() {
-        if(mainMemory.get(currProcess.getProcessName()).size() < numAllocatedFrames){
-            mainMemory.get(currProcess.getProcessName()).add(new Page(currProcess.getCurrentInstruction()));
+        GCLOCK temp = mainMemory.get(currProcess.getProcessName());
+        Page page = new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName());
+        if(temp.getBufferSize() != numAllocatedFrames){
+            temp.addNewFrame(page);
         }
         else{
-            ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
-            boolean replaced = false;
-            while(!replaced){
-                if(temp.get(currClockPointer).getReferenceCounter() == 0){
-                    temp.set(currClockPointer, new Page(currProcess.getCurrentInstruction()));
-                    replaced = true;
-                }
-                else{
-                    temp.get(currClockPointer).decReferenceCounter();
-                }
-
-                if(!replaced){
-                    if(currClockPointer == temp.size() - 1){
-                        currClockPointer = 0;
-                    }
-                    else{
-                        currClockPointer++;
-                    }
-                }
-            }
-            mainMemory.replace(currProcess.getProcessName(), temp);
-        }
+            temp.replaceCurrFrame(page);
+        }  
     }
 
     @Override
     protected boolean checkMemory(){
-        if(mainMemory.containsKey(currProcess.getProcessName())){
-            ArrayList<Page> temp = mainMemory.get(currProcess.getProcessName());
-            for(int i = 0; i < temp.size(); i++){
-                if(temp.get(i).getpageInstruction() == currProcess.getCurrentInstruction()){
-                    temp.get(i).incReferenceCounter();
-                    return true;
-                }
-            }
+        if(mainMemory.get(currProcess.getProcessName()).checkBuffer(currProcess)){
+            return true;
         }
         return false;
     }
 
     @Override
     protected void removeFromMemory() {
-        if(mainMemory.containsKey(currProcess.getProcessName())){
-            mainMemory.remove(currProcess.getProcessName());
-        }
-        
+        mainMemory.remove(currProcess.getProcessName());
     }
 }
--- src/Variable.java
+++ src/Variable.java
@@ -1,13 +1,12 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Variable extends CPU{
-    private ArrayList<Page> mainMemory;
+    private GCLOCK mainMemory;
     private int frames;
 
     public Variable(int _frames, int _timeQuantum, ArrayList<Process> _processList, String _algoName){
         super(_timeQuantum, _algoName, _processList);
-        mainMemory = new ArrayList<>();
+        mainMemory = new GCLOCK();
         frames = _frames;
     }
 
@@ -20,51 +19,27 @@ public class Variable extends CPU{
 
     @Override
     protected void loadIntoMemory() {
+        Page page = new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName());
         if(frames != 0){
-            mainMemory.add(new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName()));
+            mainMemory.addNewFrame(page);
             frames--;
         }
         else{
-            boolean replaced = false;
-            while(!replaced){
-                for(int i = 0; i < mainMemory.size(); i++){
-                    if(mainMemory.get(currClockPointer).getReferenceCounter() == 0){
-                        mainMemory.set(currClockPointer, new Page(currProcess.getCurrentInstruction(), currProcess.getProcessName()));
-                        frames--;
-                        replaced = true;
-                        break;
-                    }
-                    else{
-                        mainMemory.get(currClockPointer).decReferenceCounter();
-                    }
-
-                    if(currClockPointer == mainMemory.size() - 1){
-                        currClockPointer = 0;
-                    }
-                    else{
-                        currClockPointer++;
-                    }
-                }
-            }
+            mainMemory.replaceCurrFrame(page);
         }
-        
     }
 
     @Override
     protected boolean checkMemory() {
-        for(int i = 0; i < mainMemory.size(); i++){
-            if(mainMemory.get(i).getProcessName().equals(currProcess.getProcessName())){
-                if(mainMemory.get(i).getpageInstruction() == currProcess.getCurrentInstruction()){
-                    mainMemory.get(i).incReferenceCounter();
-                    return true;
-                }
-            }
+        if(mainMemory.checkBuffer(currProcess)){
+           return true; 
         }
         return false;
     }
 
+
     @Override
     protected void removeFromMemory() {
-        
+        return;
     }
 }
